02571eaf0dcc79f34e607d6aab29209610b6c25b
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-24 21:08:30 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const ObjectId = require('mongodb').ObjectId;
-const {MongoClient, ServerApiVersion} = require('mongodb');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json())
@@ -11,9 +10,27 @@ app.use(express.json())
 app.get('/', (req, res) =>{
     res.send('portfolio walid is testing')
 });
+//user portfoliowalid
+// password zENlRw8osW6EYNxZ
+
+
+const uri = "mongodb+srv://portfoliowalid:zENlRw8osW6EYNxZ@cluster0.ok6zpfz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("portfoliowalid").collection("service");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 async function run(){
+    try{
+      await client.connect();
+      const serviceCollection = client.db('portfoliowalid').collection('service');
+      
+    }finally{
 
+    }
 }
 run().catch(console.dir);
 

bb3d29769e4e0f022f748c93612ed6ea72288526
mollikacomputer <mollikacomputer3@gmail.com>
2023-03-12 00:10:18 +0600

    some changes

--- index.js
+++ index.js
@@ -43,13 +43,64 @@ async function run(){
         const result = await serviceCollection.deleteOne(query);
         res.send(result);
     });
-      // app.delete('/service/:id', async(req, res) =>{
-      //   const id = req.params.id;
-      //   const query = {_id:ObjectId(id)};
-      //   const result = await serviceCollection.deleteOne(query);
-      //   res.send(result);
-      // });
-      // comment collection section
+    // delete service data
+    app.delete('/comment/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await commentCollection.deleteOne(query);
+      res.send(result);
+  });
+// update comment step 1
+  app.get('/dashboard/updatecomment/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id:ObjectId(id)};
+    const result = await commentCollection.findOne(query);
+    res.send(result);
+  });
+  //update comment step 2
+  app.put('/dashboard/updatecomment/:id', async(req, res)=>{
+    const id = req.params.id;
+    const updateComment = req.body;
+    const filter = {_id:ObjectId(id)};
+    const options = {upsert:true};
+    const updateDoc={
+      $set:{
+        name: updateComment.name,
+        description: updateComment.description,
+        rating:updateComment.rating,
+        location:updateComment.location,
+        image:updateComment.image,
+      }
+    };
+    const result = await commentCollection.updateOne(filter, updateDoc, options);
+    res.send(result);
+  } )
+    // for update service step 1
+    app.get('/dashboard/updateservice/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+    // update service step 2
+      app.put('/dashboard/updateservice/:id', async(req, res)=>{
+        const id = req.params.id;
+        const updateService = req.body;
+        const filter = {_id:ObjectId(id)};
+        const options = {upsert:true};
+        const updateDoc={
+          $set:{
+            name: updateService.name,
+            description: updateService.description,
+            budget:updateService.budget,
+            image:updateService.image,
+            
+          }
+        };
+        const result = await serviceCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+      } )
+
       app.post('/comment', async(req, res) => {
         const newComment = req.body;
         console.log('adding new comment', newComment);
@@ -62,6 +113,8 @@ async function run(){
         const comment = await cursor.toArray();
         res.send(comment);
       });
+      //  comment section crud
+      
     }finally{
       
     }
